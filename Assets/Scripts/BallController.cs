e0367933e260ac5dd9f4554bf1228942fff43f01
tapafon <65124300+tapafon@users.noreply.github.com>
2025-07-03 19:17:17 +0300

    Spawning physics sequence prototyped (no colors yet)

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,18 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private BallState currentState = BallState.BeforeFixed;
+    [SerializeField] private Rigidbody rigidBody;
+
+    public void Fix(float delay = 2f)
     {
-        
+        if (currentState != BallState.BeforeFixed) return;
+        currentState = BallState.Fixed;
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.drag = 2f;
+        rigidBody.angularDrag = 2f;
+        StartCoroutine(DelayedKinematic(delay));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Release()
     {
-        
+        if (currentState != BallState.Fixed) return;
+        currentState = BallState.AfterFixed;
+        rigidBody.drag = 0f;
+        rigidBody.angularDrag = 0.05f;
+        rigidBody.isKinematic = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (currentState == BallState.Fixed)
+        {
+            var ball = other.gameObject.GetComponent<BallController>();
+            if (ball) ball.Fix();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (currentState == BallState.Fixed)
+        {
+            var ball = other.gameObject.GetComponent<BallController>();
+            if (ball) ball.Fix();
+        }
+    }
+
+    enum BallState
+    {
+        BeforeFixed,
+        Fixed,
+        AfterFixed,
+    }
+
+    IEnumerator DelayedKinematic(float delay = 0.1f)
+    {
+        yield return new WaitForSeconds(delay);
+        rigidBody.isKinematic = true;
     }
 }
--- Assets/Scripts/SpawnLine.cs
+++ Assets/Scripts/SpawnLine.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class SpawnLine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject ballsParent;
+
+    // private void OnCollisionEnter(Collision other)
+    // {
+    //     var ball = other.gameObject.GetComponent<BallController>();
+    //     if (ball) ball.Fix();
+    // }
+    //
+    // private void OnCollisionStay(Collision other)
+    // {
+    //     var ball = other.gameObject.GetComponent<BallController>();
+    //     if (ball) ball.Fix();
+    // }
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        var ball = other.gameObject.GetComponent<BallController>();
+        if (ball) ball.Fix(0f);
+        // var balls = ballsParent.GetComponentsInChildren<BallController>();
+        // foreach (var ball in balls)
+        // {
+        //     ball.Fix();
+        // }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay(Collider other)
     {
-        
+        var ball = other.gameObject.GetComponent<BallController>();
+        if (ball) ball.Fix(0f);
+        // var balls = ballsParent.GetComponentsInChildren<BallController>();
+        // foreach (var ball in balls)
+        // {
+        //     ball.Fix();
+        // }
     }
 }
